4c8ad7b6bb40323d6cbff808184377e804536c31
KerryRudoy <kerry.rudoy@gmail.com>
2019-10-25 11:44:33 +0300

    Waits added

--- src/test/java/JiraLogin.java
+++ src/test/java/JiraLogin.java
@@ -1,11 +1,15 @@
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
 
 
 public class JiraLogin {
@@ -16,6 +20,7 @@ public class JiraLogin {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kary\\JiraLogin1\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
@@ -27,7 +32,7 @@ public class JiraLogin {
         Assert.assertEquals(driver.getCurrentUrl(), "https://jira.hillel.it/secure/Dashboard.jspa");
     }
     @Test
-    public void createIssue() throws InterruptedException {
+    public void createIssue() {
         this.driver.get("https://jira.hillel.it/login.jsp");
         driver.findElement(By.xpath("//input[@name='os_username']")).sendKeys("Karina_Soltanovskaya");
         driver.findElement(By.xpath("//input[@name='os_password']")).sendKeys("Karina_Soltanovskaya");
@@ -35,14 +40,13 @@ public class JiraLogin {
         Assert.assertEquals(driver.getCurrentUrl(), "https://jira.hillel.it/secure/Dashboard.jspa");
 
         driver.findElement(By.id("create_link")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.id("project-field"));
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("project-field")));
         driver.findElement(By.id("issuetype-field"));
         driver.findElement(By.id("summary")).sendKeys("This is test");
         driver.findElement(By.xpath("//li[@data-mode='source']"));
         driver.findElement(By.xpath("//textarea[@name='description']")).sendKeys("This is test");
         driver.findElement(By.cssSelector("#create-issue-submit")).click();
-        Thread.sleep(5000);
         Assert.assertTrue(driver.findElement(By.xpath("//a[@class='issue-created-key issue-link']")).isDisplayed());
 
     }
